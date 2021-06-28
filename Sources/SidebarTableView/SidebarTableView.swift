4e93903553750f65e27b91d5fd4471c08b6a0804
Adam Kopeć <adamkopec5@icloud.com>
2021-06-28 17:18:13 +0200

    Initial commit

--- Sources/SidebarTableView/SidebarTableView.swift
+++ Sources/SidebarTableView/SidebarTableView.swift
@@ -1,145 +1,48 @@
 import UIKit
 
-open class SidebarTableView: UITableViewController, UIPointerInteractionDelegate {
-    // Helper variable
-    private var didChange = false
+open class SidebarTableView: UITableView {
     
-    /// Value on which we decide if we're currently in split view presentation
-    private var isInSplitViewPresentation: Bool {
-        return !(splitViewController?.isCollapsed ?? true)
+    convenience init() {
+        self.init(frame: .zero, style: .insetGrouped)
     }
     
-    /// The value signifying the selected row, or the row which should be selected on nearest appearance
-    /// You should only change section, as for Sidebar styled UITableView you can only have one row per section
-    open lazy var lastSelectedRow = IndexPath(row: 0, section: 0)
-    
-    /// Initializer which sets the correct `UITableView` style – `UITableView.Style.insetGrouped`
-    public override init(style: UITableView.Style) {
-        super.init(style: .insetGrouped)
+    convenience init(frame: CGRect) {
+        self.init(frame: frame, style: .insetGrouped)
     }
     
-    /// Required initializer
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: .insetGrouped)
+        commonInit()
+    }
+    
     required public init?(coder: NSCoder) {
         super.init(coder: coder)
-    }
-    
-    open override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Preserve selection between presentations
-        self.clearsSelectionOnViewWillAppear = false
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(self.renewLastSelection), name: UIApplication.willEnterForegroundNotification, object: nil)
-        
-        tableView.backgroundColor = .secondarySystemBackground
-    }
-    
-    open override func viewLayoutMarginsDidChange() {
-        super.viewLayoutMarginsDidChange()
-        if didChange {
-            // Fix navigationBar height
-            navigationController?.navigationBar.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 96.0)
-            didChange.toggle()
+        if self.style != .insetGrouped {
+            fatalError("SidebarTableView: Make sure the table view style is set to \"Inset Grouped\" in Interface Builder")
         }
+        commonInit()
     }
-    
-    open override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        // Animation
-        if let selectedRow = tableView.indexPathForSelectedRow {
-            if let coordinator = transitionCoordinator {
-                coordinator.animate(alongsideTransition: { (context) in
-                    self.tableView.deselectRow(at: selectedRow, animated: context.isAnimated)
-                }, completion: { (context) in
-                    if context.isCancelled {
-                        self.tableView.selectRow(at: selectedRow, animated: false, scrollPosition: .none)
-                    }
-                })
-            } else {
-                tableView.deselectRow(at: selectedRow, animated: animated)
-            }
-        }
+    /// Perform common initialisation operations
+    private func commonInit() {
+        self.backgroundColor = .secondarySystemBackground
+        self.separatorColor = .clear
+        self.separatorStyle = .none
     }
-    
-    open override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        // Renew Selection
-        renewLastSelection()
-        // Required for proper UINavigationBar Layout
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.largeTitleDisplayMode = .always
-        navigationController?.navigationBar.setNeedsLayout()
+    /// Pre-Configure cell before returning
+    open override func dequeueReusableCell(withIdentifier identifier: String) -> UITableViewCell? {
+        let cell = super.dequeueReusableCell(withIdentifier: identifier)
+        cell?.configureForSidebar()
+        return cell
     }
-    
-    // MARK: - Support Selection Preserving
-    
-    /// Function which selects the indexPath specified in `lastSelectedRow`
-    @objc private func renewLastSelection() {
-        if isInSplitViewPresentation && tableView.indexPathForSelectedRow == nil {
-            tableView?.selectRow(at: lastSelectedRow, animated: false, scrollPosition: .none)
-        }
-    }
-    
-    // MARK: - Pointer Interaction delegate
-    
-    @available(iOS 13.4, *)
-    open func pointerInteraction(_ interaction: UIPointerInteraction, styleFor region: UIPointerRegion) -> UIPointerStyle? {
-        var pointerStyle: UIPointerStyle? = nil
-        if let interactionView = interaction.view {
-            let targetedPreview = UITargetedPreview(view: interactionView)
-            pointerStyle = UIPointerStyle(effect: UIPointerEffect.hover(targetedPreview, preferredTintMode: .overlay, prefersShadow: true, prefersScaledContent: true))
-        }
-        return pointerStyle
-    }
-    
-    // MARK: - Table View Delegate
-    
-    /// Tells the delegate a row is selected.
-    /// - parameter tableView: A table view informing the delegate about the new row selection.
-    /// - parameter indexPath: An index path locating the new selected row in tableView.
-    open override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Set the selection
-        lastSelectedRow = indexPath
-    }
-    
-    /// Tells the delegate the table view is about to draw a cell for a particular row.
-    ///
-    /// - parameter tableView: The table view informing the delegate of this impending event.
-    /// - parameter cell: A cell that tableView is going to use when drawing the row.
-    /// - parameter indexPath: An index path locating the row in tableView.
-    open override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-//        if isLoadingTable && tableView.indexPathsForVisibleRows?.last?.row == indexPath.row && isInSplitViewPresentation {
-//            isLoadingTable = false
-//            tableView.selectRow(at: lastSelectedRow, animated: false, scrollPosition: .none)
-//        }
-        
-        if isInSplitViewPresentation {
-            // Remove any accessory
-            cell.accessoryType = .none
+}
 
-            // Set Sidebar style
-            let bgView = UIView()
-            bgView.backgroundColor = cell.tintColor
-            
-            cell.selectedBackgroundView = bgView
-            cell.textLabel?.highlightedTextColor = UIColor.white
-        } else {
-            // Make the cell regular tableView style, as we're not in SplitView anymore
-            cell.selectedBackgroundView = nil
-            cell.selectionStyle = .default
-            cell.textLabel?.highlightedTextColor = .label
-        }
-    }
-    
+public extension UITableViewCell {
     /**
-     The function configures the provided cell for correct iPadOS 14 Sidebar-like look. Call this function for each cell you initialize, creating only one cell per table view section to achieve the desired, rounded, look.
+     The function configures the provided cell for correct iPadOS 14 Sidebar-like look. Call this function for each cell you initialise to achieve the desired, rounded, look.
      
-     - parameter cell: The `UITableViewCell` which should be configured.
-     - parameter systemName: The symbol name, which should be used as the image in cell's `imageView`
-     - warning: This function has to be called for each cell you initialize!
+     - parameter cell: The ``UITableViewCell`` which should be configured.
+     - parameter systemName: The symbol name, which should be used as the image in cell's ``UITableViewCell.imageView``
+     - important: This function has to be called for each cell you initialize!
      
      ```swift
      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -149,7 +52,7 @@ open class SidebarTableView: UITableViewController, UIPointerInteractionDelegate
             cell.textLabel?.text = "Favourites"
      
             // Configure the cell for Sidebar style
-            self.configure(cell: cell, withSymbolName: "heart.fill")
+            cell.configureForSidebar(withSymbolName: "heart.fill")
      
             return cell
         } else if indexPath.section == 1 {
@@ -158,57 +61,42 @@ open class SidebarTableView: UITableViewController, UIPointerInteractionDelegate
             cell_1.textLabel?.text = "Text Only"
 
             // Configure the cell for Sidebar style
-            self.configure(cell: cell_1)
+            cell_1.configureForSidebar()
 
             return cell_1
         }
         ...
      }
      ```
-     # Notes: #
-     Remember that for the Sidebar style `UITableView` you can only have one cell (row) per section.
      */
-    public func configure(cell: UITableViewCell, withSymbolNamed systemName: String? = nil) {
+    func configureForSidebar(withSymbolNamed systemName: String? = nil) {
         // Set the symbol image
         if let systemName = systemName {
-            cell.imageView?.image = UIImage(systemName: systemName)
-            cell.imageView?.highlightedImage = UIImage(systemName: systemName)?.withTintColor(.white, renderingMode: .alwaysOriginal)
-        }
-        // Add pointer interactions
-        if #available(iOS 13.4, *) {
-            let pointerInteraction = UIPointerInteraction(delegate: self)
-            cell.addInteraction(pointerInteraction)
+            self.imageView?.image = UIImage(systemName: systemName)
+            self.imageView?.highlightedImage = UIImage(systemName: systemName)?.withTintColor(.white, renderingMode: .alwaysOriginal)
         }
         // Set the cell background color
-        cell.backgroundColor = .secondarySystemBackground
-    }
+        self.backgroundColor = .secondarySystemBackground
+        //
+        self.layer.masksToBounds = true
+        
+        self.setNeedsLayout()
+        self.layoutIfNeeded()
+        
+        self.layer.cornerRadius = 10.0
+        self.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
+        
+        // Remove any accessory
+        self.accessoryType = .none
 
-    // MARK: - Header strings for Section
-    
-    public override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return nil
+        // Set Sidebar style
+        let bgView = UIView()
+        bgView.backgroundColor = self.tintColor
+        bgView.layer.masksToBounds = true
+        bgView.layer.cornerRadius = 10.0
+        bgView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
+        
+        self.selectedBackgroundView = bgView
+        self.textLabel?.highlightedTextColor = UIColor.white
     }
-    
-    // MARK: - Height Adjustments for iPadOS 14 Sidebar-like Layout
-    
-    public override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        guard section > 0 else { return super.tableView(tableView, heightForHeaderInSection: section) }
-        return 1.0
-    }
-    
-    public override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        guard section > 0 else { return super.tableView(tableView, heightForFooterInSection: section) }
-        return 1.0
-    }
-    
-    public override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard section > 0 else { return super.tableView(tableView, viewForHeaderInSection: section) }
-        return UIView(frame: .zero)
-    }
-    
-    public override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        guard section > 0 else { return super.tableView(tableView, viewForFooterInSection: section) }
-        return UIView(frame: .zero)
-    }
-
 }
